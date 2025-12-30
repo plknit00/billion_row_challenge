5268d6a16211e7a8ecabd5b8b17f62cf27a2418c
Paige Knittel <plknit00@gmail.com>
2025-12-29 18:14:39 -0800

    testing git

--- ParseFile.java
+++ ParseFile.java
@@ -21,6 +21,7 @@ public class ParseFile {
     if (start_index == -1 || start_index + 1 >= row.length()) {
       throw new NumberFormatException("row missing ';': " + row);
     }
+    // test
     return Float.parseFloat(row.substring(start_index + 1));
   }
 
